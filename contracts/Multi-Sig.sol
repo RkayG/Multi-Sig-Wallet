8b6d6b55bf831e7a1149f3b3f1599c282697b0b4
Rufus Gladness <111110427+RkayG@users.noreply.github.com>
2024-11-10 21:41:05 +0100

    Update Multi-Sig.sol
    Basic multi-sig contract

--- contracts/Multi-Sig.sol
+++ contracts/Multi-Sig.sol
@@ -1,34 +1,76 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+const { assert } = require('chai');
+describe('MultiSig', function () {
+    let contract;
+    let accounts;
+    beforeEach(async () => {
+        accounts = await ethers.provider.listAccounts();
+        const MultiSig = await ethers.getContractFactory("MultiSig");
+        contract = await MultiSig.deploy(accounts.slice(0, 3), 1);
+        await contract.deployed();
+    });
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+    describe('storing ERC20 tokens', function () {
+        const initialBalance = 10000;
+        let token;
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+        beforeEach(async () => {
+            const EIP20 = await ethers.getContractFactory("EIP20");
+            token = await EIP20.deploy(initialBalance, 'My Token', 1, 'MT');
+            await token.deployed();
+            await token.transfer(contract.address, initialBalance);
+        });
 
-    event Withdrawal(uint amount, uint when);
+        it('should store the balance', async () => {
+            const balance = await token.balanceOf(contract.address);
+            assert.equal(balance.toNumber(), initialBalance);
+        });
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+        describe('executing an ERC20 transaction', function () {
+            beforeEach(async () => {
+                const data = token.interface.encodeFunctionData("transfer", [accounts[2], initialBalance]);
+                await contract.submitTransaction(token.address, 0, data);
+            });
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
+            it('should have removed the contract balance', async () => {
+                const balance = await token.balanceOf(contract.address);
+                assert.equal(balance.toNumber(), 0);
+            });
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+            it('should have moved the balance to the destination', async () => {
+                const balance = await token.balanceOf(accounts[2]);
+                assert.equal(balance.toNumber(), initialBalance);
+            });
+        });
+    });
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    describe('storing ether', function () {
+        const oneEther = ethers.utils.parseEther("1");
+        beforeEach(async () => {
+            await ethers.provider.getSigner(0).sendTransaction({ to: contract.address, value: oneEther });
+        });
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+        it('should store the balance', async () => {
+            const balance = await ethers.provider.getBalance(contract.address);
+            assert.equal(balance.toString(), oneEther.toString());
+        });
 
-        owner.transfer(address(this).balance);
-    }
-}
+        describe('executing the ether transaction', function () {
+            let balanceBefore;
+
+            beforeEach(async () => {
+                balanceBefore = await ethers.provider.getBalance(accounts[1]);
+                await contract.submitTransaction(accounts[1], oneEther, "0x");
+            });
+
+            it('should have removed the contract balance', async () => {
+                const balance = await ethers.provider.getBalance(contract.address);
+                assert.equal(balance, 0);
+            });
+
+            it('should have moved the balance to the destination', async () => {
+                const balance = await ethers.provider.getBalance(accounts[1]);
+                assert.equal(balance.sub(balanceBefore).toString(), oneEther.toString());
+            });
+        });
+    });
+});
